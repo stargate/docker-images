f3b256c92f5780de14dd0cbf88545ad133954e7d
Doug Wettlaufer <doug.wettlaufer@datastax.com>
2020-09-09 02:00:17 -0500

    Setup docker release framework

---
+++ cassandra-4.0/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [ -z ${1+x} ]; then 
+   echo "stargate version is a required argument"
+fi
+
+stargate_version=$1
+DOCKER_IMAGE=stargateio/stargate-4_0
+
+# TODO: Uncomment once persistence-extensions are added
+
+# echo "Building $DOCKER_IMAGE"
+# docker buildx build --push \
+# --build-arg VERSION=${VERSION} \
+# --tag ${DOCKER_IMAGE}:${stargate_version} \
+# --file Dockerfile \
+# --platform linux/amd64,linux/arm64 .
+
+# echo "Inspecting $DOCKER_IMAGE"
+# docker buildx imagetools inspect ${DOCKER_IMAGE}:${stargate_version}
