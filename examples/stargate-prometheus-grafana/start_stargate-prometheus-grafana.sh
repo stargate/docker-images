b2961b6586597c2ad5a5044144f32d94ad42de31
polandll <lorinapoland@gmail.com>
2020-11-18 14:03:13 -0800

    make changes based on Doug's comments

--- examples/stargate-prometheus-grafana/start_stargate-prometheus-grafana.sh
+++ examples/stargate-prometheus-grafana/start_stargate-prometheus-grafana.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
-COMPOSE_FILE=stargate-prometheus-grafana.yml
+#COMPOSE_FILE=stargate-prometheus-grafana.yml
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
-docker-compose --file $COMPOSE_FILE up -d backend-1
-sleep 60
+docker-compose up -d backend-1
+# Wait until the seed node is up before bringing up more nodes
+(docker-compose logs -f backend-1 &) | grep -q "is now part of the cluster"
+
 # Bring up the 2nd C* node
-#docker-compose --file $COMPOSE_FILE up -d backend-2
-#sleep 60
+(docker-compose logs -f backend-1 &) | grep -q "is now part of the cluster"
 # Bring up the 3rd C* node
-#docker-compose --file $COMPOSE_FILE up -d backend-3
-#sleep 60
+(docker-compose logs -f backend-1 &) | grep -q "is now part of the cluster"
+
 # Bring up the stargate
-docker-compose --file $COMPOSE_FILE up -d stargate
-sleep 60
+docker-compose up -d stargate
+# Wait until stargate is up before bringing up the metrics tools
+echo ""
+echo "Waiting for stargate to start up..."
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8082/health)" != "200" ]]; do
+    printf '.' 
+    sleep 5
+done
+
 # Bring up prometheus and grafana
-docker-compose --file $COMPOSE_FILE up -d prometheus grafana
+docker-compose up -d prometheus grafana
