450a84791cbf2fbf993f4f1c4496443202c76cc3
Lorina Poland <lorina@datastax.com>
2021-02-09 10:25:39 -0800

    Add start scripts for documentation (#38)
    * add start scripts to docker-compose
    
    * add codeblock to readme
    
    * add start scripts + vars in docker-compose.yaml
    
    * add warning about port mapping
    
    * make md tidier
    
    * make md tidier
    
    * remove commented lines in docker-compose
    
    * add add'l examples to readme
    
    * take out start script uncommenting
    
    * add logic to all start scripts
    
    * added wait logic to start scripts, don't need in add_zeppelin
    
    * change port 8085 to 8084
    
    * Update examples/cassandra-4.0/docker-compose.yml
    
    Co-authored-by: Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>
    
    * Update examples/cassandra-4.0/docker-compose.yml
    
    Co-authored-by: Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>
    
    * Update examples/cassandra-3.11/start_cass_311.sh
    
    Co-authored-by: Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>
    
    * Update examples/cassandra-3.11/start_cass_311.sh
    
    Co-authored-by: Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>
    
    * Update examples/cassandra-4.0/start_cass40.sh
    
    Co-authored-by: Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>
    
    * Update examples/cassandra-4.0/start_cass40.sh
    
    Co-authored-by: Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>
    
    * Update examples/dse-6.8/start_dse_68.sh
    
    Co-authored-by: Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>
    
    * Update examples/dse-6.8/start_dse_68.sh
    
    Co-authored-by: Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>
    
    Co-authored-by: Doug Wettlaufer <45750136+dougwettlaufer@users.noreply.github.com>

--- examples/cassandra-4.0/start_cass40.sh
+++ examples/cassandra-4.0/start_cass40.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 
-#COMPOSE_FILE=stargate-prometheus-grafana.yml
+export CASSTAG=4.0
+export SGTAG=v1.0.7
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
+
 docker-compose up -d backend-1
+
 # Wait until the seed node is up before bringing up more nodes
+
 (docker-compose logs -f backend-1 &) | grep -q "Created default superuser role"
 
 # Bring up the 2nd C* node
-docker-compose up -d backend-2
-(docker-compose logs -f backend-1 &) | grep -q "is now part of the cluster"
+
+docker-compose up -f $COMPOSE_FILE -d backend-2
+(docker-compose logs -f backend-2 &) | grep -q "is now part of the cluster"
+
 # Bring up the 3rd C* node
-docker-compose up -d backend-3
-(docker-compose logs -f backend-1 &) | grep -q "is now part of the cluster"
+
+docker-compose up -f $COMPOSE_FILE -d backend-3
+(docker-compose logs -f backend-3 &) | grep -q "is now part of the cluster"
 
 # Bring up the stargate
+
 docker-compose up -d stargate
 
-# Wait until stargate is up before bringing up the metrics tools
+# Wait until stargate is up before bringing up zeppelin
+
 echo ""
 echo "Waiting for stargate to start up..."
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8082/health)" != "200" ]]; do
-    printf '.' 
+    printf '.'
     sleep 5
 done
-
-# Bring up prometheus and grafana
-docker-compose up -d prometheus grafana
