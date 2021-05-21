cbac17d99541ad7d0e9877eb9fbbe8572cf663d4
Ivan Senic <ivan.senic@datastax.com>
2021-05-21 14:32:56 +0200

    modified command to support MacOS

--- bump_stargate.sh
+++ bump_stargate.sh
@@ -8,5 +8,5 @@ if [ -z ${1+x} ]; then
 fi
 
 STARGATE_VERSION=$1
-find -name "Dockerfile" -exec sed -i "s|ARG STARGATE_VERSION=.*|ARG STARGATE_VERSION=${STARGATE_VERSION}|" {} \;
-find -name "start_*.sh" -exec sed -i "s|export SGTAG=.*|export SGTAG=${STARGATE_VERSION}|" {} \;
+find . -name "Dockerfile" -exec perl -i -pe "s|ARG STARGATE_VERSION=.*|ARG STARGATE_VERSION=${STARGATE_VERSION}|" {} \;
+find . -name "start_*.sh" -exec perl -i -pe "s|export SGTAG=.*|export SGTAG=${STARGATE_VERSION}|" {} \;
