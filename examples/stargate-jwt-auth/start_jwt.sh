3b46ffaddbfec9bca0a831141f8bea79f166e650
Dmitri Bourlatchkov <40603221+dimas-b@users.noreply.github.com>
2021-06-04 10:13:12 -0400

    Use Stargate 1.0.26 (#62)

--- examples/stargate-jwt-auth/start_jwt.sh
+++ examples/stargate-jwt-auth/start_jwt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export CASSTAG=3.11.10
-export SGTAG=v1.0.25
+export SGTAG=v1.0.26
 export KCTAG=latest
 
 SG_AUTH_DIR=/Users/lorina.poland/CLONES/stargate/docker-images/examples/stargate-jwt-auth
