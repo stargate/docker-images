ca31bcc91847d5e8364005954ed81877c9616cb4
Michael Penick <michael.penick@datastax.com>
2021-12-08 08:22:32 -0500

    Use Stargate v1.0.45

--- examples/cassandra-3.11/start_cass_311.sh
+++ examples/cassandra-3.11/start_cass_311.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export CASSTAG=3.11.8
-export SGTAG=v1.0.44
+export SGTAG=v1.0.45
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
 
--- examples/stargate-jwt-auth/start_jwt.sh
+++ examples/stargate-jwt-auth/start_jwt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export CASSTAG=3.11.10
-export SGTAG=v1.0.44
+export SGTAG=v1.0.45
 export KCTAG=latest
 
 SG_AUTH_DIR=$PWD
