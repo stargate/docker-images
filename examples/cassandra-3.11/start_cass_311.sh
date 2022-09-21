88d21f4806ba29be23f106676e04fc5a31d04c7b
Eric Borczuk <eric.borczuk@datastax.com>
2022-09-20 20:26:44 -0400

    Use 1.0.65 (#116)

--- examples/cassandra-3.11/start_cass_311.sh
+++ examples/cassandra-3.11/start_cass_311.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export CASSTAG=3.11.12
-export SGTAG=v1.0.64
+export SGTAG=v1.0.65
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
 
--- examples/dse-6.8/start_dse_68.sh
+++ examples/dse-6.8/start_dse_68.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export DSETAG=6.8.20
-export SGTAG=v1.0.64
+export SGTAG=v1.0.65
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
 
