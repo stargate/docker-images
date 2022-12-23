595df58890f798c5396b9fd57e38b7ff732a61ef
Jeffrey Carpenter <jeffrey.carpenter@datastax.com>
2022-12-22 18:54:00 -0700

    updating to v1.0.71 (#123)

--- examples/cassandra-4.0/start_cass40.sh
+++ examples/cassandra-4.0/start_cass40.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export CASSTAG=4.0.3
-export SGTAG=v1.0.70
+export SGTAG=v1.0.71
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
 
--- examples/dse-6.8/start_dse_68.sh
+++ examples/dse-6.8/start_dse_68.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export DSETAG=6.8.20
-export SGTAG=v1.0.70
+export SGTAG=v1.0.71
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
 
