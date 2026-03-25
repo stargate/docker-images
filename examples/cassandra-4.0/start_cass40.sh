c3db944be85616124c0c4deceeabab31cfbd7056
Tatu Saloranta <tatu.saloranta@datastax.com>
2026-03-25 15:16:53 -0700

    Use Stargate v1.0.91 (#144)

--- examples/cassandra-4.0/start_cass40.sh
+++ examples/cassandra-4.0/start_cass40.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export CASSTAG=4.0.7
-export SGTAG=v1.0.90
+export SGTAG=v1.0.91
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
 
--- examples/dse-6.8/start_dse_68.sh
+++ examples/dse-6.8/start_dse_68.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export DSETAG=6.8.32
-export SGTAG=v1.0.90
+export SGTAG=v1.0.91
 
 # Make sure backend-1, the seed node, is up before bringing up other nodes and stargate
 
